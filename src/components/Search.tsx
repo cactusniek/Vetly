d841ee9c9a201071ba576f9c4ff80dcd88ff27ff
cactusniek <niekamperse2@proton.me>
2026-02-02 21:40:42 +0100

    update: added alphabetical order search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -7,11 +7,37 @@ import '../index.css'
 import iconSearch from '../assets/icon-search.svg'
 
 export default function Search() {
+    async function searchBar(e: any) {
+        e.preventDefault()
+    }
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    const firstRow = alphabet.slice(0, 14) // a - n
+    const secondRow = alphabet.slice(14) // o - z
+
     return (
         <div className="component-search">
-            <div className="container-search">
-                <img src={iconSearch} className="icon-search" />
-                <input type="text" value="" onChange={e => onSearch(e.target.value)} placeholder="Zoek Medicijn" className="input-search" />
+            <div className="container-search-bar">
+                <img src={iconSearch} className="icon-search-bar" />
+                <input type="text" value="" onChange={e => onSearch(e.target.value)} placeholder="Zoek Medicijn" className="input-search-bar" />
+            </div>
+
+            <div className="container-search-alphabetical">
+                <div className="alphabet-row">
+                    {firstRow.map(letter => (
+                        <span key={letter} className="alphabet-letter">
+                            {letter}
+                        </span>
+                    ))}
+                </div>
+
+                <div className="alphabet-row">
+                    {secondRow.map(letter => (
+                        <span key={letter} className="alphabet-letter">
+                            {letter}
+                        </span>
+                    ))}
+                </div>
             </div>
         </div>
     )
