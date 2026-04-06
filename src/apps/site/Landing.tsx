b0df8cb4da440a5aedfd60c9c4c4a1fb5b8af0af
cactusniek <niekamperse2@proton.me>
2026-04-06 14:34:50 +0200

    update: Landing.tsx

--- src/apps/site/Landing.tsx
+++ src/apps/site/Landing.tsx
@@ -26,16 +26,20 @@ function Landing() {
 
                 <div className="container_sections">
                     <div className="section_1">
-                        <h1 className="hero_title">
-                            Met actuele diergeneesmiddeleninformatie
-                            <br />
-                            direct op de website van uw praktijk..
-                        </h1>
-                        <h2 className="hero_subtitle">zorgt u voor overzichtelijke informatie zonder onnodig papier</h2>
+                        <div className="container_titles">
+                            <h1 className="hero_title">
+                                Met actuele diergeneesmiddeleninformatie
+                                <br />
+                                direct op de website van uw praktijk..
+                            </h1>
+
+                            <h2 className="hero_subtitle">zorgt u voor overzichtelijke informatie zonder onnodig papier</h2>
+                        </div>
 
                         <img src={VideoPlaceholder} className="VideoPlaceholder" />
                         {/* <video src="" poster="" preload="metadata" muted playsInline className=""></video> */}
 
+                        {/* Click moet naar beneden scrollen, naar alle aangesloten dierenartsen */}
                         <a href="#" target="_blank" rel="noopener noreferrer" className="button_login">
                             Widget bekijken
                         </a>
